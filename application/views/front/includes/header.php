475b24ffb96c594c419c269bbacf73df1baff15a
Armen Charkhchyan <armen.charkhchyan@ktak.am>
2024-02-07 14:03:34 +0400

    fix

--- application/views/front/includes/header.php
+++ application/views/front/includes/header.php
@@ -36,7 +36,7 @@
 	<link href="<?= base_url(); ?>assets/dist/css/select2.min.css" rel="stylesheet" type="text/css" />
 	<link href="<?= base_url(); ?>assets/dist/css/select2totree.css" rel="stylesheet" type="text/css" />
 	<link href="<?= base_url(); ?>assets/css/jquery.datetimepicker.css" rel="stylesheet" type="text/css" />
-	<link href="<?= base_url(); ?>assets/css/style.css?v=0.27" rel="stylesheet">
+	<link href="<?= base_url(); ?>assets/css/style.css?v=0.28" rel="stylesheet">
 
 	<script src="<?= base_url(); ?>assets/js/jquery.min.js"></script>
 	<script src="<?= base_url(); ?>assets/vendor/purecounter/purecounter_vanilla.js"></script>
@@ -125,12 +125,12 @@
 
 	<!-- ======= Header ======= -->
 	<header id="header" class="align-items-center d-lg-block d-flex">
-		<div class="container-fluid px-5 d-flex align-items-center justify-content-between position-relative">
+		<div class="container-fluid px-lg-5 d-flex align-items-center justify-content-between position-relative">
             <a href="<?= base_url(); ?>" class="text-black d-lg-block d-none">
                 <img src="<?php echo base_url('assets/img/text.png'); ?>" height="50" alt="text" style="object-fit: contain;" class="w-100">	
             </a>
-			<h1 class="logo d-lg-none d-block"><a href="<?= base_url(); ?>">A.C.</a></h1>			
-			<form action="<?php echo base_url('category/'); ?>" method="get" class="ms-2 me-2 me-lg-0 ms-lg-3 d-lg-none d-block">
+			<h1 class="mb-0 d-lg-none d-block"><a href="<?= base_url(); ?>">A.C.</a></h1>			
+			<form action="<?php echo base_url('category/'); ?>" method="get" class="w-100 ms-2 me-2 me-lg-0 ms-lg-3 d-lg-none d-block">
 				<select name="id" class="select2ToTree form-control" onchange="this.form.submit()"></select>
 			</form>		
 			<nav class="navbar">				
